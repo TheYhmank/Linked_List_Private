63e59f83c13c7f32f30a400e87dcf86f86fba800
Alibi <ayermeko@student.42prague.com>
2024-04-02 16:18:24 +0200

    fasdf

--- VN08/rm_elem.c
+++ VN08/rm_elem.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 typedef struct s_list
 {
@@ -18,19 +18,6 @@ void deallocate(t_list** root)
 		free(aux);
 	}
 	*root = NULL;
-}
-
-void insert_beginning(t_list** root, int value)
-{
-	t_list* new_node = malloc(sizeof(t_list));
-	if (!(new_node))
-		exit(1);
-	new_node->next = *root;
-	//Currect created node will point to the privious node
-	new_node->x = value;
-
-	*root = new_node;
-	//our root becomes new node
 }
 
 void insert_end(t_list** root, int value)
@@ -52,31 +39,31 @@ void insert_end(t_list** root, int value)
 	curr->next = new_node;
 }
 
-void add_after(t_list* root, int value)
+void rm_elem(t_list** root, int value)
 {
-	t_list *new_node = malloc(sizeof(t_list));
-	new_node->next = root->next;
-	new_node->x = value;
-
-	root->next = new_node;
-}
-
-void sort_insert(t_list** root, int value)
-{
-	if (*root == NULL || (**root).x >= value)
-	{
-		insert_beginning(root, value);
+	if (*root == NULL)
 		return ;
+
+	if ((*root)->x == value)
+	{
+		t_list* first = *root;
+		*root = (*root)->next;
+		free(first);
+		return;
 	}
 	
 	t_list* curr = *root;
 	while (curr->next)
 	{
-		if (curr->next->x >= value)
-			break;
+		if (curr->next->x == value)
+		{
+			t_list* to_remove = curr->next;
+			curr->next = curr->next->next;
+			free(to_remove);
+			return;
+		}
 		curr = curr->next;
 	}
-	add_after(curr, value);
 }
 
 int main(int argc, char const *argv[])
@@ -88,9 +75,8 @@ int main(int argc, char const *argv[])
 	insert_end(&root, -98);
 	insert_end(&root, 24);
 	insert_end(&root, 3);
-	insert_end(&root, 89);
 
-	sort_insert(&root, 99);
+	rm_elem(&root, 23);
 
 	t_list* curr = root;
 	while (curr)
